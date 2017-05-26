37c70216c67e91e6e4f311ee1f9fd98f1de9a41b
asinha94 <asinha94@gmail.com>
2017-05-26 00:01:06 -0700

    Fixed type in setup and added python dev package

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # I dont remember if 'bluetooth' is needed, but install that anyway. the dev lib is necessary to compile pybluez
-sudo apt-get install -y libbluetooth-deb bluetooth
+sudo apt-get install -y libbluetooth-dev bluetooth python2.7-dev
 
 # Install All the python packages (Update as necessary)
 pip install -r requirements.txt
 
 # Download from source and install
-git clone https://github.com/akolster/python-mindwave
+git clone https://github.com/akloster/python-mindwave
 pushd python-mindwave
 sudo python setup.py install
 popd
